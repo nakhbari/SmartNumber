75307c903c426f966f75c2a0b7e0e11811e1a687
nakhbari <nnakhbari@gmail.com>
2013-08-26 22:47:11 -0400

    RawQuery Works
    Need to implement Algorithm

--- SmartNumber/src/com/nima/akhbari/www/MainActivity.java
+++ SmartNumber/src/com/nima/akhbari/www/MainActivity.java
@@ -1,5 +1,10 @@
 package com.nima.akhbari.www;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,8 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +32,6 @@ public class MainActivity extends Activity implements NewNumberHandle,
 	EditText etNewNumber;
 	public List<String> array = new ArrayList<String>();
 	public List<String> phoneList = new ArrayList<String>();
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +45,6 @@ public class MainActivity extends Activity implements NewNumberHandle,
 		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-
-	/**
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
-	 *           menu; this adds items to the action bar if it is present.
-	 *           getMenuInflater().inflate(R.menu.main, menu); return true; }
-	 */
 
 	// Set-up for the Action Bar Drop Down Menu
 	public void setDropDownBar() {
@@ -69,28 +69,37 @@ public class MainActivity extends Activity implements NewNumberHandle,
 
 				if (position == 0) {
 					currentFragment = new NewNumberFragment();
-					ft.replace(R.id.myFragment, currentFragment);
-					ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-					ft.addToBackStack(null);
-					ft.commit();
 				} else {
 					currentFragment = new PhoneListFragment();
-					ft.replace(R.id.myFragment, currentFragment);
-					ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-					ft.addToBackStack(null);
-					ft.commit();
+					// Open Database
+					PhoneListDatabase phoneListDatabase = new PhoneListDatabase(
+							MainActivity.this);
+					SQLiteDatabase db = phoneListDatabase.getReadableDatabase();
 
-					phoneList.add("1-800-555-5555");
-					if (fragmentCommunicator != null) {
+					Cursor cursor = db.rawQuery(
+							"SELECT " + PhoneListDatabase.C_CONVERTED_NUMBER
+									+ " FROM " + PhoneListDatabase.TABLE
+									+ " WHERE " + PhoneListDatabase.C_NUMBER
+									+ " = '" + array.get(position) + "'", null);
 
-						fragmentCommunicator.passDataToFragment(phoneList);
-					} else {
-
-						Log.d("fragmentCommunicator is null",
-								"fragmentCommunicator == null");
+					phoneList.clear();
+					for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor
+							.moveToNext()) {
+						phoneList
+								.add(cursor.getString(cursor
+										.getColumnIndex(PhoneListDatabase.C_CONVERTED_NUMBER)));
 					}
+
+					// Close Database
+					db.close();
+					phoneListDatabase.close();
+
 				}
 
+				ft.replace(R.id.myFragment, currentFragment);
+				ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+				ft.addToBackStack(null);
+				ft.commit();
 				return true;
 
 			}
@@ -106,16 +115,25 @@ public class MainActivity extends Activity implements NewNumberHandle,
 			array.add(phoneNumber);
 		}
 		Log.i("phoneNumber", "The Phone Number is " + phoneNumber);
-		//Open Database
-		PhoneListDatabase phoneListDatabase = new PhoneListDatabase(MainActivity.this);
+		// Open Database
+		PhoneListDatabase phoneListDatabase = new PhoneListDatabase(
+				MainActivity.this);
 		SQLiteDatabase db = phoneListDatabase.getWritableDatabase();
-		
-		
-		
-		//Close Database
+
+		// Create Content Value
+		ContentValues values = new ContentValues();
+		values.put(PhoneListDatabase.C_NUMBER, phoneNumber);
+		values.put(PhoneListDatabase.C_IS_IN_DICTIONARY, "False");
+		values.put(PhoneListDatabase.C_IS_IN_DROPDOWN, "True");
+		values.put(PhoneListDatabase.C_CONVERTED_NUMBER, "Blah blah");
+
+		// Insert into database
+		db.insert(PhoneListDatabase.TABLE, null, values);
+
+		// Close Database
 		db.close();
 		phoneListDatabase.close();
-		
+
 	}
 
 	@Override
--- SmartNumber/src/com/nima/akhbari/www/PhoneListDatabase.java
+++ SmartNumber/src/com/nima/akhbari/www/PhoneListDatabase.java
@@ -1,8 +1,10 @@
 package com.nima.akhbari.www;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import android.util.Log;
@@ -13,7 +15,7 @@ public class PhoneListDatabase extends SQLiteOpenHelper {
 	public static final String DB_NAME = "phoneList.db";
 	public static final int DB_VERSION = 1;
 	public static final String TABLE = "PhoneList";
-	public static final String C_ID = BaseColumns._ID; // Special for id
+	public static final String C_ID = BaseColumns._ID;
 	public static final String C_NUMBER = "PhoneNumber";
 	public static final String C_IS_IN_DICTIONARY = "IsInDictionary";
 	public static final String C_IS_IN_DROPDOWN = "IsInDropdown";
@@ -26,18 +28,54 @@ public class PhoneListDatabase extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		String sql = String
-				.format("CREATE TABLE %s (%s INT, %s VARCHAR, %s VARCHAR, %s VARCHAR, %s VARCHAR);",
-						TABLE, C_ID, C_NUMBER, C_IS_IN_DICTIONARY,
-						C_IS_IN_DROPDOWN, C_CONVERTED_NUMBER);
-		Log.d(TAG,"onCreate sql: "+ sql);
+				.format("CREATE TABLE %s ( %s INT, %s VARCHAR, %s VARCHAR, %s VARCHAR, %s VARCHAR);",
+						TABLE, C_ID, C_NUMBER, C_IS_IN_DICTIONARY, C_IS_IN_DROPDOWN,
+						C_CONVERTED_NUMBER);
+		Log.d(TAG, "onCreate sql: " + sql);
 		db.execSQL(sql);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE);
-		Log.d(TAG,"onUpdate dropped table "+ TABLE);
+		Log.d(TAG, "onUpdate dropped table " + TABLE);
 		this.onCreate(db);
+	}
+
+	// Insert data
+	public boolean insertValues(String phoneNumber, String isInDictionary,
+			String isInDropdown, String convertedNumber) {
+
+		SQLiteDatabase db;
+
+		db = this.getWritableDatabase();
+		try {
+
+			ContentValues values = new ContentValues();
+
+			values.put(C_NUMBER, phoneNumber);
+			values.put(C_IS_IN_DICTIONARY, isInDictionary);
+			values.put(C_IS_IN_DROPDOWN, isInDropdown);
+			values.put(C_CONVERTED_NUMBER, convertedNumber);
+
+			long result = db.insertOrThrow(TABLE, null, values);
+			return result > 0;
+		} catch (SQLException ex) {
+			Log.w("SQLException", ex.fillInStackTrace());
+			return false;
+		} finally {
+			if (db != null) {
+				db.close();
+			}
+		}
+	}
+
+	public Cursor fetchAll(SQLiteDatabase db, String query) {
+		Cursor cursor;
+		cursor = db.rawQuery(query, null);
+
+		return cursor;
+
 	}
 
 }
